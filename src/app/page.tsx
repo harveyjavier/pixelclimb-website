b3b0057c89f640ee3efcfb9ecbce4f6617fe6fb2
Harvey Javier <harveyjavier17@gmail.com>
2024-10-08 16:27:23 +0800

    Add link to token page in Solana Devnet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,16 +34,31 @@ export default function Home() {
             PLAY NOW
           </a>
           <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-[var(--indigo)] text-background gap-2 hover:bg-[var(--gray)] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://docs.google.com/presentation/d/16RyBghsvNW822D_68EvWE-_b2ZL5Y8TkQRFif1F48lc/edit?usp=sharing"
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-[var(--blue)] text-background gap-2 hover:bg-[var(--gray)] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="https://explorer.solana.com/address/DuZq6LWkvRwLw5t2GqbSx3BeAtSeikDVf1VKraxbMLcs?cluster=devnet"
             target="_blank"
             rel="noopener noreferrer"
           >
-            ABOUT US
+            PCMB TOKEN
           </a>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="pcmb-text-gray flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://docs.google.com/presentation/d/16RyBghsvNW822D_68EvWE-_b2ZL5Y8TkQRFif1F48lc/edit?usp=sharing"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="https://nextjs.org/icons/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          LEARN MORE
+        </a>
         <a
           className="pcmb-text-gray flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://x.com/pixelclimb"
